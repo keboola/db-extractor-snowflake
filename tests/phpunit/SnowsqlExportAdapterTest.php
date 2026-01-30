b30fe9e4266a0ae376b7d447664a8c8602f19083
Ondřej Jodas <ondra.jodas@gmail.com>
2026-01-30 11:13:00 +0100

    test: add unit test for GET command trailing slash

--- tests/phpunit/SnowsqlExportAdapterTest.php
+++ tests/phpunit/SnowsqlExportAdapterTest.php
@@ -9,6 +9,7 @@ use Keboola\DbExtractor\Configuration\ValueObject\SnowflakeDatabaseConfig;
 use Keboola\DbExtractor\Extractor\SnowflakeMetadataProvider;
 use Keboola\DbExtractor\Extractor\SnowflakeQueryFactory;
 use Keboola\DbExtractor\Extractor\SnowsqlExportAdapter;
+use Keboola\DbExtractorConfig\Configuration\ValueObject\ExportConfig;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\Test\TestLogger;
 use ReflectionClass;
@@ -77,6 +78,68 @@ class SnowsqlExportAdapterTest extends TestCase
                 'my-table_123',
                 'REMOVE @~/my-table_123/;',
             ],
+        ];
+    }
+
+    /**
+     * @dataProvider generateDownloadSqlDataProvider
+     */
+    public function testGenerateDownloadSqlUsesTrailingSlash(string $tableName): void
+    {
+        $logger = new TestLogger();
+
+        $connection = $this->createMock(OdbcConnection::class);
+        $connection->method('quoteIdentifier')->willReturnCallback(fn($val) => '"' . $val . '"');
+
+        $queryFactory = $this->createMock(SnowflakeQueryFactory::class);
+        $metadataProvider = $this->createMock(SnowflakeMetadataProvider::class);
+
+        $databaseConfig = $this->createMock(SnowflakeDatabaseConfig::class);
+        $databaseConfig->method('getHost')->willReturn('test.snowflakecomputing.com');
+        $databaseConfig->method('getUsername')->willReturn('testuser');
+        $databaseConfig->method('getPassword')->willReturn('testpass');
+        $databaseConfig->method('getDatabase')->willReturn('testdb');
+        $databaseConfig->method('hasWarehouse')->willReturn(false);
+        $databaseConfig->method('hasSchema')->willReturn(false);
+        $databaseConfig->method('hasPrivateKey')->willReturn(false);
+
+        $adapter = new SnowsqlExportAdapter(
+            $logger,
+            $connection,
+            $queryFactory,
+            $metadataProvider,
+            $databaseConfig,
+        );
+
+        $exportConfig = $this->createMock(ExportConfig::class);
+        $exportConfig->method('getOutputTable')->willReturn($tableName);
+
+        $reflection = new ReflectionClass($adapter);
+        $method = $reflection->getMethod('generateDownloadSql');
+        $method->setAccessible(true);
+        $method->invoke($adapter, $exportConfig, '/tmp/output');
+
+        // Check that the debug log contains GET command with trailing slash
+        $debugLogs = array_filter($logger->records, fn($record) => $record['level'] === 'debug');
+        $this->assertNotEmpty($debugLogs, 'Expected debug log to be recorded');
+
+        $logMessage = end($debugLogs)['message'];
+        $expectedGetCommand = sprintf('GET @~/%s/ file:///tmp/output;', $tableName);
+        $this->assertStringContainsString(
+            $expectedGetCommand,
+            $logMessage,
+            sprintf('Expected GET command with trailing slash for table "%s"', $tableName),
+        );
+    }
+
+    public function generateDownloadSqlDataProvider(): array
+    {
+        return [
+            'simple table name' => ['my_table'],
+            'table name with prefix that could match other tables' => ['r_executive_brand_revenue'],
+            'table name that is prefix of another' => ['orders'],
+            'table name with dots' => ['in.c-main.escaping'],
+            'table name with special characters' => ['my-table_123'],
         ];
     }
 }
